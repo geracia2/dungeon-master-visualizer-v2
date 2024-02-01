1fd0d50908ebe502afa758c98617eef91ddb4e76
Aaron Geraci <geracia2@gmail.com>
2024-01-31 22:09:03 -0500

    adjusted api to work better

--- client/Api.js
+++
@@ -1,2 +0,0 @@
-const baseURL = import.meta.env.PROD ? import.meta.env.VITE_BASE_URL : "";
-export default baseURL;
---
+++ client/src/Api.js
@@ -0,0 +1,3 @@
+const baseURL = import.meta.env.VITE_BASE_URL ? import.meta.env.VITE_BASE_URL : "http://localhost:5000";
+export default baseURL;
+ 
